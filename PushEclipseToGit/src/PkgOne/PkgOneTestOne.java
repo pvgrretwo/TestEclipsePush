2589a8a04aebcf30443d29ec1c158895bb23c106
Cigniti <Cigniti@DESKTOP-NEM1N47>
2020-02-26 15:57:24 +1100

    Commit Three

---
+++ PushEclipseToGit/src/PkgOne/PkgOneTestOne.java
@@ -0,0 +1,18 @@
+package PkgOne;
+
+public class PkgOneTestOne {
+	
+
+	
+	public void pkonemethodone() {
+		System.out.println("pkonemethodone accessed");
+		int a = 10;
+		String s1 = "This is first string";
+	}
+	
+	public void multiply (int a, int b) {
+		int multi = a*b;
+		System.out.println(multi);
+	}
+
+}
---
+++ PushEclipseToGit/src/TestTwo.java
@@ -0,0 +1,15 @@
+
+public class TestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(2,3);
+
+	}
+	
+	public static void sum (int a, int b) {
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}

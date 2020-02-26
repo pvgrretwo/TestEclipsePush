5ed0c1d2a9f39e4ab31849a6cb48cbf2c2b28f08
Cigniti <Cigniti@DESKTOP-NEM1N47>
2020-02-26 15:56:37 +1100

    Second commit

--- PushEclipseToGit/src/TestOne.java
+++ PushEclipseToGit/src/TestOne.java
@@ -1,10 +1,25 @@
+import PkgOne.PkgOneTestOne;
 
 public class TestOne {
+	
+	int i;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("New Line Code");
+		System.out.println("This has new lines");
+		PkgOneTestOne t1 = new PkgOneTestOne();
+		t1.pkonemethodone();
+		TestTwo w = new TestTwo();
+		w.sum(5, 6);
+	
 
+	}
+	
+	public void one () {
+		System.out.println("I am first method in Default Package TestOne");
+		
 	}
 
 }
